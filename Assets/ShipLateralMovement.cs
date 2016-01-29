3dc01f8cf36706b76f56434b66a86046c7947991
Shree Dhamija <shreedhamija@gmail.com>
2016-01-29 02:03:03 -0800

    added ships and background,movement

---
+++ Assets/ShipLateralMovement.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipLateralMovement : MonoBehaviour {
+    void Update() {
+        transform.Translate(Time.deltaTime, 0, 0, Camera.main.transform);
+    }
+}
